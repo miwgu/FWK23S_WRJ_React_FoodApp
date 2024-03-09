5b4ba46a463b1a4cb259bc988e53dbeb620bf6fb
Miwa G <miwa.guhres@hotmail.com>
2024-03-09 19:25:47 +0100

    If there is no favorit show messages Save your favorites meals...., hasFavorit

--- src/components/meal/Meal_Favorites.jsx
+++ src/components/meal/Meal_Favorites.jsx
@@ -1,28 +1,40 @@
 import React, { useState,  useEffect } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 const Meal_Favorites = () => {
     
     const favorites = JSON.parse(localStorage.getItem('favorites'))||[];
-    /*const [hasFavorit, setHasFavorit] = useState(false);
-    if (favorites.length> 0){
-        setHasFavorit(true);
-    }
+    const [hasFavorit, setHasFavorit] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         // Check if there are favorites when the component mounts
         setHasFavorit(favorites.length > 0);
     }, [favorites]);
 
-*/
+    const handleGotoHome = () =>{
+        navigate('/');
+      };
+
   return (
     <>
-    {/*  {!hasFavorit && <h3>Save your favorites meals</h3> } */}
+      {!hasFavorit && 
+      <>
+        <h3>Save your favorites meals</h3>
+        <p>
+        You haven't saved anything yet. <br /> Click on the heart symbol under the meals and they will appear here.
+        </p>
+        <Button variant="primary" onClick={handleGotoHome }>Search for more meals</Button>
+        </>
+         }
     <ul>
         {favorites.map((idMeal)=>(
-         <Link to ={`/meal-details/${idMeal}`} style={{textDecoration: 'none'}}>
-         <li key={idMeal}>{idMeal}</li>
-         </Link>
+        <li key={idMeal}>
+          <Link to ={`/meal-details/${idMeal}`} style={{textDecoration: 'none'}}>
+            {idMeal}
+          </Link>
+         </li>
         ))}
     </ul>
     </>
